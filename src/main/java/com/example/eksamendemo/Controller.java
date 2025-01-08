2c3fc66e19f97bafe06449ad72dd58adfe295704
Gerberg1 <144436875+Gerberg1@users.noreply.github.com>
2025-01-08 10:33:51 +0100

    hej

--- src/main/java/com/example/eksamendemo/Controller.java
+++ src/main/java/com/example/eksamendemo/Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-    @GetMapping("")
+    @GetMapping("/")
     public String hello(){
         return "index";
     }
